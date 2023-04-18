90cdea8407dbf3b3812f0cc5ad524007a54d8758
Sochetra Nov <sochetra.nov@Sochetra-NOV.local>
2023-04-18 15:22:25 +0700

    feat: testing remove <Head />

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,33 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Analytics } from "@vercel/analytics/react";
 
+export function reportWebVitals(metric) {
+  switch (metric.name) {
+    case "FCP": {
+      // handle FCP results
+      console.log("FCP: ", metric);
+      break;
+    }
+
+    case "LCP":
+      // handle LCP results
+      break;
+    case "CLS":
+      // handle CLS results
+      break;
+    case "FID":
+      // handle FID results
+      break;
+    case "TTFB":
+      // handle TTFB results
+      break;
+    case "INP":
+      // handle INP results (note: INP is still an experimental metric)
+      break;
+    default:
+      break;
+  }
+}
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
